a4f9254e67d09e8e419b189ddc9e537f9ee16dac
PragnyaDesh <60569711+PragnyaDesh@users.noreply.github.com>
2020-02-02 18:17:43 +0530

    Initial Commit

---
+++ hello world/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello world
+//
+//  Created by Prashant Deshpande on 02/02/20.
+//  Copyright © 2020 PragnyaDesh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
